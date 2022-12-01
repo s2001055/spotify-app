9ab96978bccd3f6fb24959d720f3d7eb65dd132d
CABBY <rauhamaa.tomi@gmail.com>
2022-12-01 10:19:18 +0200

    Pieniä korjauksia

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = ({ accessToken }) => {
             <Card>
                 <Card.Header>Tietoa sivusta</Card.Header>
                 <Card.Body>
-                    <Card.Text>Sivun tarkoituksena on hakea Spotify API:n avulla artisteja, albumeita ja soittolistoja. Sivuilla pystyy myös navigoimaan artistin Spotify sivulle sekä soittamaan artistin tekemiä albumeita ja soittolistoja. Voit hyödyntää alla olevia linkkejä tai käyttää navigointipalkkia.</Card.Text>
+                    <Card.Text>Sivulla pystyy hakemaan Spotify API:n avulla artisteja, albumeita ja soittolistoja. Sivuilla pystyy myös navigoimaan artistin Spotify sivulle, sekä soittamaan artistin tekemiä albumeita. Voit halutessa hyödyntää alla olevia linkkejä tai käyttää navigointipalkkia.</Card.Text>
                     <Link to="/artists" className="btn btn-primary btn-sm card-link">Hae artisteja</Link>
                     <Link to="/albums" className="btn btn-primary btn-sm card-link">Hae albumeita</Link>
                     <Link to="/playlists" className="btn btn-primary btn-sm card-link">Hae soittolistoja</Link>
